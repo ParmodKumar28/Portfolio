5dc7e246269ffa9618c63bd791e3730ad93b56c9
Parmod Kumar <yparmodyadav@gmail.com>
2024-04-12 14:05:48 +0530

    profile updated

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,12 +1,12 @@
 import "./Hero.css"
 import AnchorLink from "react-anchor-link-smooth-scroll"
 import resume from "../../assets/my_resume.pdf"
-import profile_img from "../../assets/profile_img.jpg"
+import profile from "../../assets/profile.jpg"
 
 const Hero = () => {
     return (
         <div id='home' className='hero'>
-            <img src={profile_img} alt="" />
+            <img src={profile} alt="" />
             <h1><span>I'm Parmod Kumar,</span> Full stack developer based in INDIA.</h1>
             <p>Hey there, I’m Parmod Yadav, a tech enthusiast with an insatiable hunger for knowledge and an unyielding passion for all things automobiles. </p>
             <div className="hero-action">
