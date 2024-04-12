8f74491747a842e1a21c6de4aea20b01e9d87c51
Parmod Kumar <yparmodyadav@gmail.com>
2024-04-12 11:28:31 +0530

    Social Links

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import './Footer.css';
+import { AiFillInstagram, AiFillLinkedin, AiFillTwitterSquare, AiFillGithub, AiFillYoutube } from 'react-icons/ai';
+import { SiLeetcode } from "react-icons/si";
 import footer_logo from "../../assets/footer_logo.svg";
 import user_icon from "../../assets/user_icon.svg";
 
@@ -29,13 +31,16 @@ const Footer = () => {
                     2024 Parmod Kumar. All rights reserved
                 </p>
                 <div className="footer-bottom-right">
-                    <p>Term of Servuces</p>
-                    <p>Privacy Policy</p>
-                    <p>Connect with me</p>
+                    <a href="https://www.instagram.com/parmodyadav28/" target="_blank" rel="noopener noreferrer"><AiFillInstagram style={{ color: '#bc2a8d', fontSize: '2.5rem' }} /></a>
+                    <a href="https://www.linkedin.com/in/parmod-kumar-9a176b202/" target="_blank" rel="noopener noreferrer"><AiFillLinkedin style={{ color: '#0077b5', fontSize: '2.5rem' }} /></a>
+                    <a href="https://twitter.com/ParmodKuma35112" target="_blank" rel="noopener noreferrer"><AiFillTwitterSquare style={{ color: '#1da1f2', fontSize: '2.5rem' }} /></a>
+                    <a href="https://leetcode.com/parmodkumar28/" target="_blank" rel="noopener noreferrer"><SiLeetcode style={{ color: '#f89f1b', fontSize: '2.5rem' }} /></a>
+                    <a href="https://github.com/ParmodKumar28" target="_blank" rel="noopener noreferrer"><AiFillGithub style={{ color: '#24292e', fontSize: '2.5rem' }} /></a>
+                    <a href="https://www.youtube.com/@parmodyadav28" target="_blank" rel="noopener noreferrer"><AiFillYoutube style={{ color: '#c4302b', fontSize: '2.5rem' }} /></a>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Footer
+export default Footer;
